d342e8960fdc8db33b5935408a9c4bf8be5956ba
Saksham Puri <sakshampuri1999@gmail.com>
2020-11-01 06:32:09 +0530

    Using the context from index file of Components which was provided by provider from App main component to get Authentication state and mould the navigator state of nested navigators

--- app/src/Authentication/index.tsx
+++ app/src/Authentication/index.tsx
@@ -5,23 +5,41 @@ import { Routes } from "../Components/Routes";
 import Onboarding from "./Onboarding";
 import Welcome from "./Welcome";
 import Login from "./Login";
-import { FirebaseContext, firebaseContextInfo } from "../Components";
+import {
+    AuthContext,
+    FirebaseContext,
+    firebaseContextInfo,
+} from "../Components";
+import { HomeNavigator } from "../Home";
 
 const AuthenticationStack = createStackNavigator<Routes>();
 
 export const AuthenticationNavigator = () => {
+    const { state: loginState } = React.useContext(AuthContext).authState;
     return (
         <FirebaseContext.Provider value={firebaseContextInfo}>
             <AuthenticationStack.Navigator headerMode='none'>
-                <AuthenticationStack.Screen
-                    name='Onboarding'
-                    component={Onboarding}
-                />
-                <AuthenticationStack.Screen
-                    name='Welcome'
-                    component={Welcome}
-                />
-                <AuthenticationStack.Screen name='Login' component={Login} />
+                {loginState !== "login" ? (
+                    <>
+                        <AuthenticationStack.Screen
+                            name='Onboarding'
+                            component={Onboarding}
+                        />
+                        <AuthenticationStack.Screen
+                            name='Welcome'
+                            component={Welcome}
+                        />
+                        <AuthenticationStack.Screen
+                            name='Login'
+                            component={Login}
+                        />
+                    </>
+                ) : (
+                    <AuthenticationStack.Screen
+                        name='Home'
+                        component={HomeNavigator}
+                    />
+                )}
             </AuthenticationStack.Navigator>
         </FirebaseContext.Provider>
     );
