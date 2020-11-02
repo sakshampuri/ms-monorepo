fd2ef9f04a64021d5c21ad46436d78685638dfca
Saksham Puri <sakshampuri1999@gmail.com>
2020-11-02 21:48:52 +0530

    main moods entry point router

---
+++ server/API/moods.py
@@ -0,0 +1,17 @@
+from API.models import Mood
+from API.moods_data import mood_data
+from API.playlistRouter import playlist
+
+from typing import List
+from fastapi import Header, APIRouter
+
+moods = APIRouter()
+
+@moods.get('/',response_model=List[Mood])
+async def index():
+    return mood_data
+
+moods.include_router(playlist)
+
+
+
