19d6dd58201606060f8c3b415339adc4e42fbec6
Saksham Puri <sakshampuri1999@gmail.com>
2020-11-10 13:19:12 +0530

    Configured audio player state to update with AVPlaybackStatusUpdate from expo-av

--- app/src/AudioPlayer/Player.tsx
+++ app/src/AudioPlayer/Player.tsx
@@ -1,21 +1,35 @@
 import * as React from "react";
-import { Animated, Dimensions, StyleSheet } from "react-native";
+import {
+    ActivityIndicator,
+    Animated,
+    Dimensions,
+    StyleSheet,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Box } from "../Restyle";
 import { Audio } from "expo-av";
 import { showErr } from "../Components";
 import AudioIcons, { PLAYER_HEIGHT } from "./AudioIcons";
 import { EvilIcons } from "@expo/vector-icons";
+import { playerActions, playerStateType } from "./types";
+import {
+    checkPermissionsAsync,
+    fadeAnimation,
+    _onPlaybackStatusUpdate,
+} from "./Utility";
+import { AVPlaybackSource } from "expo-av/build/AV";
 
 interface Props {
     visible: boolean;
-    changePlayerVisible: React.Dispatch<React.SetStateAction<boolean>>;
+    dispatch: React.Dispatch<playerActions>;
+    currentState: playerStateType["currentState"];
 }
 
 export enum actionType {
     stepbackward = "GO_BACK",
     playcircleo = "PLAY_PAUSE",
     stepforward = "GO_FORWARD",
+    pause = "PLAY_PAUSE",
 }
 
 const { width } = Dimensions.get("window");
@@ -26,54 +40,58 @@ const songs = [
     "https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3",
 ];
 
-const icons = ["stepbackward", "playcircleo", "stepforward"];
+const icons = ["stepbackward", "pause", "stepforward"];
 
 const playMusicAsync = async (
-    instance: React.MutableRefObject<Audio.Sound>
+    musicInstance: React.MutableRefObject<Audio.Sound>,
+    currentIndex: React.MutableRefObject<number>,
+    dispatch: React.Dispatch<playerActions>
 ) => {
-    await instance.current.loadAsync({ uri: songs[0] });
-    await instance.current.playAsync();
+    if (!musicInstance.current) {
+        musicInstance.current = new Audio.Sound();
+    } else {
+        await musicInstance.current.unloadAsync();
+        musicInstance.current = null;
+    }
+    const source: AVPlaybackSource = {
+        uri: songs[currentIndex.current],
+    };
+    console.log("current Index: ", currentIndex.current);
+    const { sound } = await Audio.Sound.createAsync(
+        source,
+        { shouldPlay: false },
+        _onPlaybackStatusUpdate(dispatch),
+        true
+    );
+    musicInstance.current = sound;
 };
 
-const checkPermissionsAsync = async () => {
-    return Audio.getPermissionsAsync()
-        .then((response) => {
-            if (!response.granted) {
-                Audio.requestPermissionsAsync()
-                    .then((response) => {
-                        if (!response.granted) {
-                            showErr("Permission Not Granted");
-                            return false;
-                        } else return true;
-                    })
-                    .catch(() => false);
-            } else return true;
-        })
-        .then((granted) => {
-            if (granted) {
-                Audio.setAudioModeAsync({ staysActiveInBackground: true });
-            }
-            return granted;
-        })
-        .catch(() => false);
-};
-
-const Player: React.FC<Props> = ({ visible = false, changePlayerVisible }) => {
+const Player: React.FC<Props> = ({ dispatch, visible, currentState }) => {
     const insets = useSafeAreaInsets();
 
     //Animation Config
-    let y = new Animated.Value(PLAYER_HEIGHT);
+    let y = React.useRef<Animated.Value>(new Animated.Value(PLAYER_HEIGHT));
+    let opacity = React.useRef<Animated.Value>(new Animated.Value(1));
 
-    const musicInstance = React.useRef(new Audio.Sound());
+    const fadeOut = fadeAnimation({
+        animatedInstance: opacity,
+        toValue: 0.5,
+    });
+    const fadeIn = fadeAnimation({
+        animatedInstance: opacity,
+        toValue: 1,
+    });
+
+    // Music Main Instance
+    const musicInstance = React.useRef<Audio.Sound>(new Audio.Sound());
+    const currentIndex = React.useRef<number>(0);
 
     React.useEffect(() => {
         if (visible) {
-            Animated.spring(y, {
+            Animated.spring(y.current, {
                 toValue: 0,
                 useNativeDriver: true,
-            }).start(() => {
-                y = new Animated.Value(PLAYER_HEIGHT);
-            });
+            }).start();
 
             //Permission Check
             checkPermissionsAsync()
@@ -81,28 +99,68 @@ const Player: React.FC<Props> = ({ visible = false, changePlayerVisible }) => {
                     if (!granted) {
                         throw new Error("Could not get Permission");
                     } else {
-                        playMusicAsync(musicInstance).catch((err) => {
-                            showErr(err.toString());
-                        });
+                        playMusicAsync(
+                            musicInstance,
+                            currentIndex,
+                            dispatch
+                        ).catch((err) => showErr(err.toString()));
                     }
                 })
                 .catch((err) => console.log(err));
-        } else
+        } else {
             musicInstance.current
-                .unloadAsync()
+                ?.unloadAsync()
                 .then((response) => console.log(response))
                 .catch((err) => console.log(err));
+            y.current = new Animated.Value(PLAYER_HEIGHT);
+        }
     }, [visible]);
 
+    React.useEffect(() => {
+        switch (currentState) {
+            case "buffering":
+                fadeOut.start();
+                break;
+            case "playing":
+                fadeIn.start();
+                break;
+            case "paused":
+                musicInstance.current?.pauseAsync();
+        }
+    }, [currentState]);
+
     //Icon Press Actions
-    const handlePlayerIconPress = (action: actionType) => () => {};
+    const handlePlayerIconPress = (action: actionType) => {
+        switch (action) {
+            case "PLAY_PAUSE":
+                currentState === "paused"
+                    ? musicInstance.current?.playAsync()
+                    : musicInstance.current?.pauseAsync();
+                break;
+            case "GO_BACK":
+                {
+                    if (currentIndex.current > 0) {
+                        currentIndex.current--;
+                        playMusicAsync(musicInstance, currentIndex, dispatch);
+                    } else showErr("You're at the beginning of the playlist");
+                }
+                break;
+            case "GO_FORWARD": {
+                if (currentIndex.current < songs.length) {
+                    currentIndex.current++;
+                    playMusicAsync(musicInstance, currentIndex, dispatch);
+                } else showErr("Reached End of Playlist");
+            }
+        }
+    };
 
     return (
         <>
             {visible && (
                 <Animated.View
                     style={{
-                        transform: [{ translateY: y }],
+                        transform: [{ translateY: y.current }],
+                        opacity: opacity.current,
                         width,
                         height: PLAYER_HEIGHT,
                         paddingBottom: insets.bottom,
@@ -116,7 +174,18 @@ const Player: React.FC<Props> = ({ visible = false, changePlayerVisible }) => {
                         px='m'
                     >
                         {icons.map((name, index) => {
-                            const inconSize = name === "playcircleo" ? 30 : 24;
+                            const inconSize = name === "pause" ? 30 : 24;
+                            if (name === "pause")
+                                if (currentState === "buffering")
+                                    return (
+                                        <ActivityIndicator
+                                            color='white'
+                                            style={{ flex: 1 }}
+                                            key={index}
+                                        />
+                                    );
+                                else if (currentState === "paused")
+                                    name = "playcircleo";
                             return (
                                 <AudioIcons
                                     iconName={name}
@@ -137,7 +206,7 @@ const Player: React.FC<Props> = ({ visible = false, changePlayerVisible }) => {
                             left: width - 28,
                             paddingTop: 5,
                         }}
-                        onPress={() => changePlayerVisible(false)}
+                        onPress={() => dispatch({ type: "close" })}
                     />
                 </Animated.View>
             )}
