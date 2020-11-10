e5368281c238ea6595b5e33ef6eea5284674a391
Saksham Puri <sakshampuri1999@gmail.com>
2020-11-10 13:51:40 +0530

    Path fixed along with data parsing error

--- server/API/playlistRouter.py
+++ server/API/playlistRouter.py
@@ -15,11 +15,13 @@ async def servePlaylist(id: int):
 
 @playlist.get('/{moodId}/stream/{songId}')
 async def serveSong(moodId: int, songId: int):
-    songs=[data for data in song_data if data['mId'] == moodId]
-    for data in songs:
-        if data['id']==songId:
-            name=data['title']
-    name=name+".mp3"
-    song_path=os.path.dirname(os.path.realpath(name))
+    songs = [data for data in song_data if data['mId'] == moodId]
+    print(songs)
+    for data in songs[0]['songs']:
+        if data['id'] == songId:
+            name = data['title']
+    name = name+".mp3"
+    song_path = os.path.dirname(os.path.realpath(name))
+    song_path += "/API/data/"+name
     song = open(song_path, mode='rb')
     return StreamingResponse(song, media_type='audio/mp3')
